65d195b854594c86a41e9a5f2c2502ae68bf68f3
Henrik Hjelm <108104736+henrikhjelmse@users.noreply.github.com>
2025-01-03 21:16:21 +0000

    fix for new ha

--- custom_components/bensinpriser/__init__.py
+++ custom_components/bensinpriser/__init__.py
@@ -8,9 +8,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
