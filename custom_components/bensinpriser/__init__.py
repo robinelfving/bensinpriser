5c0e7b76bd70a12b0b6b1a1c2c6eecc9658fa9cf
robinelfving <84117304+robinelfving@users.noreply.github.com>
2025-01-12 19:33:12 +0100

    Update __init__.py

--- custom_components/bensinpriser/__init__.py
+++ custom_components/bensinpriser/__init__.py
@@ -8,7 +8,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
